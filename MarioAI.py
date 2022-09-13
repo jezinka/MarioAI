13dbc137c3a71ca34c70bedb4d19314fa704cd26
jezinka <jezina1989@gmail.com>
2022-09-13 11:25:43 +0200

    learning loading playing

--- MarioAI.py
+++ MarioAI.py
@@ -1,15 +1,35 @@
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from nes_py.wrappers import JoypadSpace
+from gym.wrappers import GrayScaleObservation
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+from matplotlib import pyplot as plt
+import os
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
+
+from callback import TrainAndLoggingCallback
 
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
+env = GrayScaleObservation(env, keep_dim=True)
+env = DummyVecEnv([lambda: env])
+env = VecFrameStack(env, 4, channels_order='last')
 
-done = True
-for step in range(10000):
-    if done:
-        state = env.reset()
-    state, reward, done, info = env.step(env.action_space.sample())
+CHECKPOINT_DIR = './train/'
+LOG_DIR = './logs/'
+
+callback = TrainAndLoggingCallback(check_freq=10000, save_path=CHECKPOINT_DIR)
+
+# AI model started
+model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=0.000001, n_steps=512)
+
+model.learn(total_timesteps=1000000, callback=callback)
+
+model.load('./train/best_model_1')
+
+state = env.reset()
+while True:
+    action, _ = model.predict(state)
+    state, reward, done, info = env.step(action)
     env.render()
-
-env.close()
