96248039f742cd43c283271691bf531fb9faf745
jezinka <jezina1989@gmail.com>
2022-09-13 12:31:50 +0200

    encapsulate in functions

--- MarioAI.py
+++ MarioAI.py
@@ -1,35 +1,37 @@
 import gym_super_mario_bros
+from gym.wrappers import GrayScaleObservation
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from nes_py.wrappers import JoypadSpace
-from gym.wrappers import GrayScaleObservation
-from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
-from matplotlib import pyplot as plt
-import os
 from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
 
 from callback import TrainAndLoggingCallback
 
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
-env = GrayScaleObservation(env, keep_dim=True)
-env = DummyVecEnv([lambda: env])
-env = VecFrameStack(env, 4, channels_order='last')
 
-CHECKPOINT_DIR = './train/'
-LOG_DIR = './logs/'
+def create_env():
+    env = gym_super_mario_bros.make('SuperMarioBros-v0')
+    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+    env = GrayScaleObservation(env, keep_dim=True)
+    env = DummyVecEnv([lambda: env])
+    env = VecFrameStack(env, 4, channels_order='last')
+    return env
 
-callback = TrainAndLoggingCallback(check_freq=10000, save_path=CHECKPOINT_DIR)
 
-# AI model started
-model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=0.000001, n_steps=512)
+def create_model(model_env):
+    # AI model started
+    return PPO('CnnPolicy', model_env, verbose=1, tensorboard_log='./logs/', learning_rate=0.000001, n_steps=512)
 
-model.learn(total_timesteps=1000000, callback=callback)
 
-model.load('./train/best_model_1')
+def learn_model(model):
+    model.learn(total_timesteps=1000000, callback=TrainAndLoggingCallback(check_freq=10000, save_path='./train/'))
 
-state = env.reset()
-while True:
-    action, _ = model.predict(state)
-    state, reward, done, info = env.step(action)
-    env.render()
+
+if __name__ == '__main__':
+    play_env = create_env()
+    model = create_model(play_env)
+    model.load('./train/best_model_40000')
+    state = play_env.reset()
+    while True:
+        action, _ = model.predict(state)
+        state, reward, done, info = play_env.step(action)
+        play_env.render()
